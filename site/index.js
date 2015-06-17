89a55bf74d01f18f3ac335dfd027f988a4567490
Marco Lau <lau.career@gmail.com>
2015-06-17 12:52:34 -0400

    Add logo and fortunes

--- site/index.js
+++ site/index.js
@@ -5,16 +5,26 @@ var app = express();
 var handlebars = require('express3-handlebars').
   create({ defaultLayout:'main' });
 
+var fortunes = [
+ "Conquer your fears or they will conquer you.",
+ "Rivers need springs.",
+ "Do not fear what you don't know.",
+ "You will have a pleasant surprise.",
+ "Whenever possible, keep it simple.",
+];
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.render('about');
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
 });
 
 // custom 404 page
